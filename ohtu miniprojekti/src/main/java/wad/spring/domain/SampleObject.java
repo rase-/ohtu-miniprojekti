02d213450dd35abc5f71c7ec3becee07bb130ed3
Tony Kovanen <tonykovanen@hotmail.com>
2012-03-30 11:15:00 +0300

    uusittu pohja

---
+++ ohtu miniprojekti/src/main/java/wad/spring/domain/SampleObject.java
@@ -0,0 +1,21 @@
+package wad.spring.domain;
+
+public class SampleObject {
+
+    private boolean awesome;
+
+    public SampleObject() {
+    }
+
+    public SampleObject(boolean awesome) {
+        this.awesome = awesome;
+    }
+
+    public boolean isAwesome() {
+        return awesome;
+    }
+
+    public void setAwesome(boolean awesome) {
+        this.awesome = awesome;
+    }
+}
