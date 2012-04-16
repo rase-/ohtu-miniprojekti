0d3152b50b0890578e82972a1f3555117dd2f709
Herkko Virolainen <Herkko.virolainen@gmail.com>
2012-04-16 11:41:53 +0300

    parseri muutoksia

--- ohtu_mini/src/main/java/wad/spring/tools/parsers.java
+++ ohtu_mini/src/main/java/wad/spring/tools/parsers.java
@@ -1,16 +1,18 @@
 
 package wad.spring.tools;
 
+import java.util.List;
 import wad.spring.domain.Reference;
 
 public class parsers {
     
-    
+    //Anni
     public static Reference parseScandit(Reference toBeParsed){
         
         return toBeParsed;
     }
     
+    //Markus
     public static Reference parsePageNumber(Reference toBeParsed){
         
         
@@ -18,14 +20,16 @@ public class parsers {
     }
     
     public static Reference generateCite(Reference toBeGenerated){
-        String firstLetter = toBeGenerated.getAuthor();
-        firstLetter = firstLetter.substring(0, 1);
-        
-        String year = toBeGenerated.getPublishingYear();
-        year = year.substring(2);
-        String palautettava = firstLetter + year;
-        
-        toBeGenerated.setReferenceCite(palautettava);
+        String firstLetter = toBeGenerated.getAuthor().substring(0,1);
+        String year = toBeGenerated.getPublishingYear().substring(2);
+        toBeGenerated.setReferenceCite(firstLetter + year);
         return toBeGenerated;
+    }
+    
+    //Herkko
+    public static boolean checkCites(List<Reference> list){
+        //tarkistetaan ettei yksikään Cite koodi ole samanlainen ja jos on, niin korvataan se uudella.
+        
+        return true;
     }
 }
