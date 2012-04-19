f369b9084e1e4809c6e6180d94e69f6a5b343da2
Tony Kovanen <tonykovanen@hotmail.com>
2012-04-19 20:04:28 +0300

    BibTeX servicen generateString metodista korjattu toStringien yhdistämistä niin että tekee kkokonaisen rivin välin jokaisen entryn välille

--- ohtu_mini/src/main/java/wad/spring/service/BibtexServiceImpl.java
+++ ohtu_mini/src/main/java/wad/spring/service/BibtexServiceImpl.java
@@ -12,41 +12,41 @@ import org.springframework.stereotype.Service;
 import wad.spring.repository.ReferenceRepository;
 import wad.spring.tools.Parsers;
 import wad.spring.domain.Reference;
+
 /**
  *
  * @author tonykova
  */
 @Service
 public class BibtexServiceImpl implements BibtexService {
+
     @Autowired
     ReferenceRepository referenceRepository;
-    
+
     @Override
     public String generateBibtex() {
-        
+
         List<Reference> allReferences = referenceRepository.findAll();
         HashSet citeCodes = new HashSet();
-        
-        for (int i=0; i<allReferences.size(); i++){
+
+        for (int i = 0; i < allReferences.size(); i++) {
             Parsers.parsePageNumber(allReferences.get(i));
-            Parsers.parseScandit(allReferences.get(i));   
+            Parsers.parseScandit(allReferences.get(i));
             Parsers.generateCite(allReferences.get(i), citeCodes);
         }
-        
+
         return generateString(allReferences);
     }
-    
-    
-    public static String generateString(List<Reference> AllReferences){
+
+    public static String generateString(List<Reference> allReferences) {
         String palautettava = "";
-        
-        for (int i=0; i<AllReferences.size(); i++){
-          String yhdistettava = AllReferences.get(i).toString();
-           palautettava = palautettava + yhdistettava; 
+
+        for (int i = 0; i < allReferences.size(); i++) {
+            String yhdistettava = allReferences.get(i).toString();
+            palautettava = palautettava + "\n\n" + yhdistettava;
         }
-        
-        
+
+
         return palautettava;
     }
-    
 }
