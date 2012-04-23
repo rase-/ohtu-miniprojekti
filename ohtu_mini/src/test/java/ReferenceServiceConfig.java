de9c683e6f7fe631f4452e191344895522af43c5
Tony Kovanen <tonykovanen@hotmail.com>
2012-04-23 10:18:34 +0300

    Pari testiä tagaykselle

--- ohtu_mini/src/test/java/ReferenceServiceConfig.java
+++ ohtu_mini/src/test/java/ReferenceServiceConfig.java
@@ -7,6 +7,7 @@
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import wad.spring.service.ReferenceService;
+import wad.spring.service.ReferenceServiceImpl;
 
 /**
  *
